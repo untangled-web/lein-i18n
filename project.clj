5e79c25eaf9851957ec6ce280225508aac922903
Tony Kay <twkay@thenavisway.com>
2016-02-12 14:44:53 -0800

    Story ID: develop ported versions

--- project.clj
+++ project.clj
@@ -5,18 +5,20 @@
             :url  "http://www.thenavisway.com"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [untangled-spec "0.1.1"]
+                 [untangled-spec "0.3.1" :scope "test"]
                  [leiningen-core "2.5.3"]
                  [lein-cljsbuild "1.1.0"]
                  [leiningen "2.5.3"]
                  ]
 
+  :repositories [["releases" "https://artifacts.buehner-fry.com/artifactory/release"]
+                 ["snapshot" "https://artifacts.buehner-fry.com/artifactory/internal-snapshots"]
+                 ["third-party" "https://artifacts.buehner-fry.com/artifactory/internal-3rdparty"]]
+
   :source-paths ["src"]
   :test-paths ["spec"]
 
-  :test-refresh {
-                 :report untangled-spec.report/untangled-report
-                 }
+  :test-refresh {:report untangled-spec.reporters.terminal/untangled-report}
 
   :profiles {
              :dev {
