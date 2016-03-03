9bd432679ed1052f2aa753d8611f443aea60cbb3
Tony Kay <twkay@thenavisway.com>
2016-03-03 09:36:00 -0800

    Story ID: master fixed license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject navis/untangled-lein-i18n "0.1.2"
   :description "A plugin for extracting/populating transalations for Untangled"
   :url ""
-  :license {:name "NAVIS"
-            :url  "http://www.thenavisway.com"}
+  :license {:name "MIT"
+            :url  "https://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [untangled-spec "0.3.1" :scope "test"]
